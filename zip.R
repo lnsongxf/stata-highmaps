da5178fbc4e400f9e27065edd7b9106d953ffebe
Painter <957028646czx@gmail.com>
2020-11-28 19:45:03 +0800

    修改链接错误

--- zip.R
+++ zip.R
@@ -13,6 +13,6 @@ dir_ls('code-data-zip') %>%
   str_remove("code-data-zip/") %>% 
   str_remove(".zip") %>% 
   as_tibble() %>% 
-  mutate(下载链接 = paste0("https://czxa.github.io/code-data-zip/", value, ".zip")) %>% 
+  mutate(下载链接 = paste0("https://czxa.github.io/stata-highmaps/code-data-zip/", value, ".zip")) %>% 
   mutate(下载链接 = paste0("<a href='", basename(下载链接), "'>", value, "</a>")) %>% 
   knitr::kable()
